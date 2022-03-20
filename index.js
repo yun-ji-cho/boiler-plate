5b789079644911f8b7fa5aa64d6637cae96cc3fb
Cho-yunji <favorcho@hanmail.net>
2022-03-20 23:17:12 +0900

    index.js 서버 생성 및 데이터 전송 테스트 완료

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const express = require('express') //익스프레스 모듈을 가져온다
 const app = express() //
 const port = 5000
-const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
 
-mongoose.connect('mongodb+srv://yunji:<dudgus0519>@boilerplate.szdfv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-  useNewUrlParser : true, useUnifiedTopology : true, useCreateIndex : true, useFindAndModify : false, // 이걸 써줘야 에러가 안뜸
-}).then(() => console.log('MongoDB Connected...'))
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended : true})); //bodyParser 는 클라이언트에서 오는 정보를 서버에서 분석해서 가져올 수 있게 한다.
+//application/json json 타입으로 된것을 분석해서 가져올 수 있게.
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://yunji:dudgus0519@boilerplate.szdfv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, res) => {res.send('Hello World!')})
+
+//회원가입을 위한 라우트
+app.post('/register', (req, res) => {
+  //회원가입 할 때 필요한 정보들을 client 에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body) //req.body 에 들어올 수 있도록 해주는 것이 bodyParser가 있어서 그렇다. 이것이 클라이언트의 정보를 받아준다.
+    user.save((err, userInfo) => { //save 는 몽고 디비의 메서드
+      if(err) return res.json({success : false, err}) //에러는 제이슨 형식으로 전달, err  메세지도 함께 전달
+      return res.status(200).json({
+        success : true
+      })
+    })
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port, () => {console.log(`Example app listening on port ${port}`)})
 
