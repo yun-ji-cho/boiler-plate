81d4808d5383046bda3e6a5c967db9bf95ad728f
Cho-yunji <favorcho@hanmail.net>
2022-04-02 18:39:27 +0900

    reac-router-dom 설치 및 디렉터리 구축

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Route,
+  Routes,} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ 
+
+  import LandingPage from './components/views/LandingPage/LandingPage'
+  import LoginPage from './components/views/LoginPage/LoginPage'
+  import RegisterPage from './components/views/RegisterPage/RegisterPage'
+
+ 
+  function App() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element = {<LandingPage/>}/>
+          <Route exact path="/login" element = {<LoginPage/>}/>
+          <Route exact path="/register" element = {<RegisterPage/>}/>
+        </Routes>
+      </BrowserRouter>
+
+    );
+
+ 
+
+  }
 
 export default App;
